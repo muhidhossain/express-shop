8aff78b40bf352926da93fcf9c27d78ec3485072
Muhid Hossain <muhidhossain7@gmail.com>
2021-01-10 19:22:48 +0600

    3rd assignment done

---
+++ controllers/user.controller.js
@@ -0,0 +1,133 @@
+const userService = require('../services/user.service');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+module.exports.isAuthenticated = async (req, res, next) => {
+    try {
+        const verified = await jwt.verify(
+            req.headers.token,
+            process.env.JWT_SECRET
+        );
+        console.log('JWT', verified);
+        if (!verified) {
+            return res
+                .status(400)
+                .json({
+                    error: true,
+                    data: null,
+                    token: null,
+                    message: 'user',
+                });
+        }
+        next();
+    } catch (err) {
+        console.error(err);
+        return res
+            .status(500)
+            .json({
+                error: err.message,
+                data: null,
+                token: null,
+                message: 'something went wrong',
+            });
+    }
+};
+
+hashPassword = (password, saltRound) => {
+    return new Promise((resolve, reject) => {
+        bcrypt.hash(password, saltRound, (err, hash) => {
+            if (err) reject(err);
+            resolve(hash);
+        });
+    });
+};
+
+module.exports.register = async (req, res, next) => {
+    try {
+        const { body } = req;
+        const saltRound = 10;
+        body.password = await hashPassword(body.password, saltRound);
+        const user = await userService.createUser(body);
+        const userObj = JSON.parse(JSON.stringify(user));
+        delete userObj.password;
+
+        const token = await jwt.sign(
+            {
+                data: userObj,
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: '24h',
+            }
+        );
+
+        return res.status(200).json({
+            error: false,
+            data: null,
+            token: token,
+            message: 'registration completed',
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({
+            error: err,
+            data: null,
+            token: null,
+            message: 'something went wrong',
+        });
+    }
+};
+
+comparePassword = (password, hash) => {
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(password, hash, (err, match) => {
+            if (err) reject(err);
+            resolve(match);
+        });
+    });
+};
+
+module.exports.login = async (req, res, next) => {
+    try {
+        const user = await userService.findUserByEmail(req.body.email);
+        const matchPassword = await comparePassword(
+            req.body.password,
+            user.password
+        );
+        console.log(matchPassword);
+        if (!matchPassword) {
+            return res.status(400).json({
+                error: false,
+                data: null,
+                token: null,
+                message: "User credential didn't matched",
+            });
+        }
+        const userObj = JSON.parse(JSON.stringify(user));
+        delete userObj.password;
+
+        const token = await jwt.sign(
+            {
+                data: userObj,
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: '24h',
+            }
+        );
+        return res.status(200).json({
+            error: false,
+            data: null,
+            token: token,
+            message: 'login successful',
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({
+            error: err,
+            data: null,
+            token: null,
+            message: 'something went wrong',
+        });
+    }
+};
--- routes/shops.js
+++ routes/shops.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const shopController = require('../controllers/shop.controller');
+const userController = require('../controllers/user.controller');
 
 /* GET users listing. */
 router.post('/', shopController.create);
 router.get('/', shopController.getAll);
 router.get('/:id', shopController.getById);
-router.put('/:id', shopController.updateById);
-router.delete('/:id', shopController.deleteById);
+router.put('/:id', userController.isAuthenticated, shopController.updateById);
+router.delete(
+    '/:id',
+    userController.isAuthenticated,
+    shopController.deleteById
+);
 
 module.exports = router;
