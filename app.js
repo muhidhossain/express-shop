6351d5f78c0496e2d8f776f340f197844eedc986
Muhid Hossain <muhid@namyata.com>
2021-01-27 23:27:37 +0600

    express-validator added

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const db = require('./db/db');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const shopsRouter = require('./routes/shops');
+const { isAuthenticated } = require('./controllers/user.controller');
 
 const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+// app.use(isAuthenticated);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/shops.js
+++ routes/shops.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const shopController = require('../controllers/shop.controller');
-const userController = require('../controllers/user.controller');
+const { shopValidator } = require('../middlewares/shop.validator');
+const { checkInvalid } = require('../middlewares/validationResult');
 
 /* GET users listing. */
-router.post('/', shopController.create);
+router.post('/', shopValidator, checkInvalid, shopController.create);
 router.get('/', shopController.getAll);
 router.get('/:id', shopController.getById);
-router.put('/:id', userController.isAuthenticated, shopController.updateById);
-router.delete(
-    '/:id',
-    userController.isAuthenticated,
-    shopController.deleteById
-);
+router.put('/:id', shopController.updateById);
+router.delete('/:id', shopController.deleteById);
 
 module.exports = router;
